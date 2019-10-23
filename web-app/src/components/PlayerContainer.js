89ec174ed52a6d1f0fbfe7cf96f9e59202cc9f0f
suraj <braindeadpool@gmail.com>
2019-10-23 14:58:12 +0100

    add progress bar

--- web-app/src/components/PlayerContainer.js
+++ web-app/src/components/PlayerContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { PlayerIcon } from 'react-player-controls';
+import { ProgressBar } from './ProgressBar';
 
 export default class PlayerContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isPlaying:false};
+        this.state = {
+            isPlaying: false,
+            currentTimeMilliseconds: 0,
+            songDurationMilliseconds: 0,
+            bufferedMilliseconds: 0,
+        };
     }
 
     render() {
@@ -16,6 +22,7 @@ export default class PlayerContainer extends React.Component {
         }
         return (
             <div>
+                <ProgressBar amountBuffered={this.state.bufferedMilliseconds / this.state.songDurationMilliseconds} currentTime={this.state.currentTimeMilliseconds * 1000} duration={this.state.songDurationMilliseconds * 1000} /><br />
                 {playPauseIcon}
                 <PlayerIcon.Previous width={32} height={32} style={{ marginRight: 32 }} />
                 <PlayerIcon.Next width={32} height={32} style={{ marginRight: 32 }} />
