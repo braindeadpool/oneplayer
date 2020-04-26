7732147765dc3a159bdfe775f697f67fa3023302
suraj <braindeadpool@gmail.com>
2020-04-26 22:56:17 +0100

    add interface api

--- libsamp/src/interfaces.ts
+++ libsamp/src/interfaces.ts
@@ -1,8 +1,70 @@
+import { PlaybackState } from './state';
+
+/**
+ * MediaProviderState exposes the relevant state of a single media provider. This is used for sending control signals back to the specific players.
+ *
+ * @interface MediaProviderState
+ */
+interface MediaProviderState {
+    currentTrackIndex: number;
+    currentTrackTimeMilliseconds: number;
+}
+
+/**
+ * MediaProvider is an interface implemented by an SDK or a wrapper around an SDK of every media player supported by libSAMP.
+ * MediaProvider lets libSAMP (and by extension its users) manipulate multiple media sources together as a single state
+ * without worrying about the specific internal implementation details.
+ *
+ * @export
+ * @interface MediaProvider
+ */
 export interface MediaProvider {
-    /* init() is always called first before actuall using the provider. All the auth and other setup details should happen here.
-     It returns whether the MediaProvider was successfully initialized. 
-  */
-    init(): boolean;
+    /**
+     * init() is always called first before actuall using the provider. All the auth and other setup details should happen here.
+     * It returns a promise boolean indicating whether the MediaProvider was successfully initialized.
+     *
+     * @returns {Promise<boolean>}
+     * @memberof MediaProvider
+     */
+    init(): Promise<boolean>;
+
+    getCurrentState(): Promise<MediaProviderState>;
+
+    /**
+     * play() starts the playback from the current track time and returns a promise indicating whether it succeeded in playing.
+     *
+     * @returns {Promise<boolean>}
+     * @memberof MediaProvider
+     */
+    play(): Promise<boolean>;
+
+    /**
+     * pause() pauses the playback at the current track time and returns a promise indicating whether it succeeded in pausing.
+     *
+     * @returns {Promise<boolean>}
+     * @memberof MediaProvider
+     */
+    pause(): Promise<boolean>;
+
+    /**
+     * togglePlayPause() plays/pauses the playback at the current track time and returns a promise indicating whether it succeeded in toggling.
+     *
+     * @returns {Promise<boolean>}
+     * @memberof MediaProvider
+     */
+    togglePlayPause(): Promise<boolean>;
+
+    seek(): Promise<number>;
+
+    setVolume(): Promise<number>;
+
+    getVolume(): Promise<number>;
+
+    next(): Promise<boolean>;
+
+    previous(): Promise<boolean>;
+
+    goToIndex(index: number): Promise<boolean>;
 }
 
 /**
