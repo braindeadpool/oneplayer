586f44f9bce01c349682b4ad8dee50bf6ea772c0
suraj <19975305+braindeadpool@users.noreply.github.com>
2021-01-12 02:37:10 +0000

    youtube search and play works

--- libsamp/src/metadataproviders/youtubeMetadata.ts
+++ libsamp/src/metadataproviders/youtubeMetadata.ts
@@ -1,3 +1,4 @@
+import { parse, toSeconds } from 'iso8601-duration';
 import axios, { AxiosInstance } from 'axios';
 import { ITrackInfo, IMetadataProvider, PlayableTrack } from '../interfaces';
 import { YouTubeProvider } from '../mediaproviders/youtubeProvider';
@@ -21,15 +22,27 @@ export class YouTubeMetadata implements IMetadataProvider {
     }
 
     getTrackInfo(trackID: string): Promise<YouTubeTrackInfo> {
-        return this._webAPIAxiosInstance.get(`videos/${trackID}`).then((response) => {
-            return {
-                source: trackID,
-                trackName: response.data.name,
-                durationInMilliseconds: response.data.duration_ms,
-                artistName: response.data.artists[0].name,
-                imageURL: response.data.album.images[0].url,
-            };
-        });
+        return this._webAPIAxiosInstance
+            .get('videos/', {
+                params: {
+                    id: `${trackID}`,
+                    part: 'snippet, contentDetails',
+                    key: this._apiKey,
+                },
+            })
+            .then((response) => {
+                return this._makeTrackInfoFromResponseData(response.data.items);
+            });
+    }
+
+    _makeTrackInfoFromResponseData(responseData: any) {
+        return {
+            source: responseData.id,
+            trackName: responseData.snippet.title,
+            durationInMilliseconds: toSeconds(parse(responseData.contentDetails.duration)) * 1000,
+            artistName: responseData.snippet.channelTitle,
+            imageURL: responseData.snippet.thumbnails['default'].url,
+        };
     }
 
     search(query: string): Promise<PlayableTrack[]> {
@@ -44,14 +57,38 @@ export class YouTubeMetadata implements IMetadataProvider {
                 },
             })
             .then((response) => {
-                let trackResults = new Array<PlayableTrack>();
-
                 if (response.status == 200) {
+                    let commaSeparatedTrackIDs = '';
                     for (let track of response.data.items) {
-                        trackResults.push(this._getPlayableTrackFromSearchResult(track));
+                        commaSeparatedTrackIDs += track.id.videoId + ',';
                     }
+
+                    // now fetch the contentDetails (this contains the track duration info)
+                    return this._webAPIAxiosInstance
+                        .get('videos/', {
+                            params: {
+                                id: `${commaSeparatedTrackIDs}`,
+                                part: 'snippet, contentDetails',
+                                key: this._apiKey,
+                            },
+                        })
+                        .then((response) => {
+                            let trackResults = new Array<PlayableTrack>();
+                            if (response.status == 200) {
+                                for (let track of response.data.items) {
+                                    trackResults.push(this._getPlayableTrackFromSearchResult(track));
+                                }
+                            }
+                            return trackResults;
+                        })
+                        .catch((response) => {
+                            console.log('Fetching video contentDetails failed with response', response);
+                            return new Array<PlayableTrack>();
+                        });
+                } else {
+                    console.log('YouTube search failed with response', response);
+                    return Array<PlayableTrack>();
                 }
-                return trackResults;
             })
             .catch((response) => {
                 console.log('YouTube search failed with response', response);
@@ -60,13 +97,7 @@ export class YouTubeMetadata implements IMetadataProvider {
     }
 
     _getPlayableTrackFromSearchResult(trackData: any): PlayableTrack {
-        const trackInfo = {
-            source: trackData.id.videoId,
-            trackName: trackData.snippet.title,
-            durationInMilliseconds: trackData.duration_ms,
-            artistName: '',
-            imageURL: trackData.snippet.thumbnails['default'].url,
-        };
+        const trackInfo = this._makeTrackInfoFromResponseData(trackData);
         return this._youtubeProvider.makePlayableTrack(trackInfo, trackData.id.videoId);
     }
 }
